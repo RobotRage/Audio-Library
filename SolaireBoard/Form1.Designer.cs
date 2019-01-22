a44f8fa1979c814184188dd096beeff1bc6929fc
Dylan Robertson <RobotRage@users.noreply.github.com>
2019-01-22 22:29:08 +1000

    First commit

---
+++ SolaireBoard/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SolaireBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btns0 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtNow = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btns0
+            // 
+            this.btns0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btns0.Location = new System.Drawing.Point(50, 52);
+            this.btns0.Name = "btns0";
+            this.btns0.Size = new System.Drawing.Size(154, 29);
+            this.btns0.TabIndex = 2;
+            this.btns0.Text = "Stop";
+            this.btns0.UseVisualStyleBackColor = false;
+            this.btns0.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(259, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(237, 206);
+            this.panel1.TabIndex = 28;
+            // 
+            // txtNow
+            // 
+            this.txtNow.Location = new System.Drawing.Point(4, 26);
+            this.txtNow.Name = "txtNow";
+            this.txtNow.Size = new System.Drawing.Size(249, 20);
+            this.txtNow.TabIndex = 29;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.Location = new System.Drawing.Point(313, 3);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(144, 17);
+            this.lblError.TabIndex = 9;
+            this.lblError.Text = "Press |> to play song.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Now Playing";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 29);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(4, 212);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(249, 20);
+            this.txtSearch.TabIndex = 32;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 29);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Search ( full name )";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(50, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 29);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "Shuffle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(501, 238);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNow);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btns0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MusicPlayer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btns0;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtNow;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SolaireBoard/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+namespace SolaireBoard
+{
+    
+    public partial class Form1 : Form
+    {
+
+        System.Media.SoundPlayer fat_beats;
+        System.IO.StreamReader settings;
+
+        string[] lines = new string[999];
+        string line;
+        int count = 0;
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+            settings = new System.IO.StreamReader(@"custom.txt");
+
+         
+            DirectoryInfo music_dir = new DirectoryInfo(@"Music");
+            FileInfo[] Files = music_dir.GetFiles("*.wav");
+            string[] names = new string[999];
+
+            int q = 0;
+            foreach(FileInfo file in Files)
+            {
+                names[q] = file.Name;
+                //  Debug.WriteLine(names[q]);
+                //rtb1.Text += file.Name;
+                q++;
+            }
+
+            while ((line = settings.ReadLine()) != null )
+            {
+                if (line != "")
+                {
+                    lines[count] = line;
+                    count++;
+                }
+            }
+
+            System.IO.File.WriteAllLines(@"all.txt", names);
+
+            btns0.Click += new EventHandler(stop);
+            int t = 0;
+            int f = 0;
+            for (int i = 0; i < count; i++)
+            {
+                
+                TextBox newText = new TextBox();
+                {
+                    newText.Name = string.Format("txt" + i);
+
+                    newText.Location = new System.Drawing.Point(118 -70, 0 + f);
+                    newText.Size = new System.Drawing.Size(169, 20);
+                    panel1.Controls.Add(newText);
+                  //  newButton.Click += new EventHandler(ButtonHandler);
+                }
+
+                f += 35;
+                Button newButton = new Button();
+                {
+                    newButton.Name = string.Format("btn" + i);
+                    newButton.Text = string.Format("|>");
+                    newButton.Location = new System.Drawing.Point(70 -70 , 0 + t);
+                    newButton.Size = new System.Drawing.Size(24, 26);
+                    newButton.BackColor = Color.LimeGreen;
+                    panel1.Controls.Add(newButton);
+                    newButton.Click += new EventHandler(ButtonHandler);
+                }
+                t += 35;
+            }
+
+            for (int i = 0; i < count ; i ++)
+            {
+                string txt = "txt" + i;
+
+                TextBox texts = this.Controls.Find(txt, true).FirstOrDefault() as TextBox;
+
+                texts.Text = lines[i];
+            }
+        }
+
+        #region line1
+
+
+        void stop(object sender, System.EventArgs e)
+        {
+            fat_beats.Stop();
+        }
+        void ButtonHandler(object sender, System.EventArgs e)
+        {
+
+
+            string ass = ((Button)sender).Name;
+           // lblError.Text = ass;
+            var result = Regex.Match(ass, @"\d+$").Value;
+            //  char last = ass[ass.Length - 1];
+            int a = int.Parse(result);
+            
+            Debug.WriteLine(a);
+            string lol = lines[a];
+        
+            try
+            {
+                txtNow.Text = lol;
+                fat_beats = new System.Media.SoundPlayer(@"Music\" + lol);     // set up media player with txt1.Text being the name of the file in root
+                fat_beats.Play();
+
+                lblError.Text = "Press |> to play song";
+                lblError.BackColor = Color.White;
+            }
+
+
+            catch
+            {
+                lblError.Text = "Error - File not found";
+                lblError.BackColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fat_beats.Stop();
+            txtNow.Text = "none";
+        }
+        #endregion
+        int rand;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            rand  = rnd.Next(0, count);
+            try
+            {
+                fat_beats = new System.Media.SoundPlayer(@"Music\" + lines[rand]);     // set up media player with txt1.Text being the name of the file in root
+                fat_beats.Play();
+                
+                lblError.Text = "Press |> to play song";
+                lblError.BackColor = Color.White;
+            }
+            catch { lblError.Text = "Error - FIle not found";
+                lblError.BackColor = Color.Red;
+            }
+            txtNow.Text = lines[rand];
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+
+                fat_beats = new System.Media.SoundPlayer(@"Music\" + txtSearch.Text);     // set up media player with txt1.Text being the name of the file in root
+                fat_beats.Play();
+                txtNow.Text = txtSearch.Text;
+
+                lblError.Text = "Press |> to play song";
+                lblError.BackColor = Color.White;
+            }
+            catch
+            {
+                txtNow.Text = "none";
+                lblError.Text = "Error - File not found";
+                lblError.BackColor = Color.Red;               
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rend = new Random();
+            string[] newAry = new string[999];
+            newAry = lines.OrderBy(x => rend.Next()).ToArray();
+            settings.Close();
+            File.Create(@"custom.txt").Close();
+            newAry = newAry.Where(x => !string.IsNullOrEmpty(x)).ToArray(); // removes white space
+            System.IO.File.WriteAllLines(@"custom.txt", newAry);
+
+
+            System.Diagnostics.Process.Start(@"SolaireBoard.exe");
+            this.Close();
+
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
